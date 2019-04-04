a2b24656dad36a18fd1d14682eeb3de4038811ae
Marcelo Dias <mardsystems@gmail.com>
2019-04-04 00:22:33 -0300

    Firebase Cloud Funcions init.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,12 @@
-import * as functions from 'firebase-functions';
+// import * as functions from 'firebase-functions';
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
